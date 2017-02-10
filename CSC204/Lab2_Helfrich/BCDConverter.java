66473951834412f883274a0d164160cbd64c254f
Unknown <orch99@gmail.com>
2017-02-10 17:54:59 -0500

    Fixed Lab2 CSC 204

--- CSC204/Lab2_Helfrich/BCDConverter.java
+++ CSC204/Lab2_Helfrich/BCDConverter.java
@@ -3,6 +3,9 @@ public class BCDConverter {
 	public static String toBCDJava (String inputStr) {
 		int baseInt;
 		int baseDec;
+		int i;
+		boolean decZeroFlag = false;
+		String leadZeros = "";
 		StringBuilder sbDec = new StringBuilder("");
 		StringBuilder sbTemp = new StringBuilder("");
 		StringBuilder sbInt = new StringBuilder("");
@@ -20,7 +23,17 @@ public class BCDConverter {
 			}
 			baseDec = Integer.parseInt(base10Temp[1]);
 			
-			while (baseInt > 0) { //left of decimal point
+			//catch if zero
+			if (baseInt == 0) {
+				sbInt.append("00");
+			}
+			if (baseDec == 0) {
+				sbDec.append(" 0");
+				decZeroFlag = true;
+			}
+			
+			
+			while (baseInt > 0) { //right of decimal point
 				a = baseInt % 10;
 				sbTemp.append(BCDConverter.intToBinaryString(a));
 				while (sbTemp.length() < 4) {
@@ -43,8 +56,17 @@ public class BCDConverter {
 				baseDec /= 10;
 				sbTemp.setLength(0);
 			}
+			//catch leading zeros after decimal
+			leadZeros = base10Temp[1];
+			i = 0;
+			while (leadZeros.charAt(i) == '0' && !decZeroFlag) {
+				sbDec.insert(0, " 0000");
+				++i;
+			}
 			
-			sbInt.append(" .");
+			sbInt.append(".");
+			sbDec.delete(0,1);
+			
 			sbInt.append(sbDec.toString());
 			
 			//formatting
@@ -54,16 +76,21 @@ public class BCDConverter {
 		}
 		else {
 			baseInt = Integer.parseInt(inputStr);
-			while (baseInt > 0) { //left of decimal point
-				a = baseInt % 10;
-				sbTemp.append(BCDConverter.intToBinaryString(a));
-				while (sbTemp.length() < 4) {
-					sbTemp.insert(0, "0");
+			if (baseInt == 0) {
+				sbInt.append("00");
+			}
+			else {
+				while (baseInt > 0) { //left of decimal point
+					a = baseInt % 10;
+					sbTemp.append(BCDConverter.intToBinaryString(a));
+					while (sbTemp.length() < 4) {
+						sbTemp.insert(0, "0");
+					}
+					sbTemp.insert(0, " ");
+					sbInt.insert(0, sbTemp.toString());
+					baseInt /= 10;
+					sbTemp.setLength(0);
 				}
-				sbTemp.insert(0, " ");
-				sbInt.insert(0, sbTemp.toString());
-				baseInt /= 10;
-				sbTemp.setLength(0);
 			}
 			//formatting
 			sbInt.delete(0, 1);
@@ -74,11 +101,9 @@ public class BCDConverter {
 	private static String intToBinaryString(int inputInt) {
 		int quotient;
 		int remainder;
-		//int outputInt = 0;
 		String outputIntR;
 		String outputIntString = "";
-		;
-
+		
 		while (inputInt != 0) {
 			quotient = inputInt / 2;
 			remainder = inputInt % 2;
